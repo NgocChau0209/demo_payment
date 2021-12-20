3b789c8b156565069f033bcdec2a878cd22dd570
NgocChau0209 <ngocchautrantp@gmail.com>
2021-12-20 08:21:58 +0700

    side bar component

--- components/Layouts/Sidebar/Sidebar.tsx
+++ components/Layouts/Sidebar/Sidebar.tsx
@@ -1,27 +1,61 @@
-import { sidebarList } from "./data";
+import { useRef } from 'react';
+import { sidebarList } from './data';
 import { useRouter } from 'next/router';
-import styles from "./Sidebar.module.scss";
-import classes from "classnames";
+import { useDispatch, useSelector } from "react-redux";
+import { setExtendSidebar } from "controller/redux/action/layout";
+import classnames from "classnames";
+import styles from './Sidebar.module.scss';
 
-export default function Sidebar(){
-    const router = useRouter()
-    function goToNextPage(url){
+export default function Sidebar() {
+    const router = useRouter();
+    const dispatch = useDispatch();
+    const isExtendSidebar = useSelector(state => state.layout.isExtendSidebar) || false;
+    /* ref to element */
+    const sidebarRef = useRef(null);
+    const closeBtnRef = useRef(null);
+
+    /* handle extend sidebar */
+    function expandSidebar() {
+        let sidebar = sidebarRef.current;
+        let closeBtn = closeBtnRef.current;
+        if (!sidebar || !closeBtn) return;
+        sidebar.classList.toggle("open");
+        /* retrict or extend menu bar */
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+        /* change global state */
+        dispatch(setExtendSidebar());
+    }
+
+    function goToNextPage(url: string) {
         router.push(url);
     }
-    return(
-        <>
-            {sidebarList.map((item,index)=>{
-                return(
-                    <div className={styles.item} onClick={()=>goToNextPage(item.url)} key={index}>
-                        <div className={classes(styles.icon,'tag-20')}>
-                            {item.text}
-                        </div>
-                        <div className={styles.text}>
-                            {item.icon}
-                        </div>
-                    </div>
-                )
-            })}
-        </>
+
+
+    return (
+        <div className={classnames(styles.sidebar, isExtendSidebar ? styles.open : "")} ref={sidebarRef}>
+            <div className={styles.logo_details}>
+                {isExtendSidebar ? <i className='bx bxl-c-plus-plus icon'></i> : ""}
+                {isExtendSidebar ? <div className={classnames(styles.logo_name, "title-18")}>CodingLab</div> : ""}
+                <i className='bx bx-menu' id={styles.btn} ref={closeBtnRef} onClick={expandSidebar}></i>
+            </div>
+            <ul className={styles.nav_list}>
+                {sidebarList.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a className={styles.links} onClick={() => goToNextPage(item.url)}>
+                                <i className={classnames('bx', item.icon)}></i>
+                                <span className={styles.links_name}>{item.text}</span>
+                            </a>
+                            <span className={classnames(styles.tooltip, "text-14")}>{item.text}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
+
--- components/Layouts/Sidebar/data.ts
+++ components/Layouts/Sidebar/data.ts
@@ -1,39 +1,35 @@
-import { TransactionIcon } from "../../Icon/Sidebar/Transaction";
-import { CardIcon } from "../../Icon/Sidebar/Card";
-import { UserIcon } from "../../Icon/Sidebar/User";
-import { StatisIcon } from "../../Icon/Sidebar/Statis";
-import { HomeIcon } from "../../Icon/Sidebar/Home";
 export let sidebarList = [
-  
   {
     text: "Trang chủ",
     url: "/",
-    icon: HomeIcon,
-  },
-  
-  {
-    text: "Thống kê",
-    url: "/statistical",
-    icon: StatisIcon,
-  },
-  {
-    text: "Quản lí thẻ",
-    url: "/cards",
-    icon: CardIcon,
-  },
-  {
-    text: "Tài khoản",
-    url: "/account/profile",
-    icon: UserIcon,
+    icon: "bx-grid-alt",
   },
   {
     text: "Giao dịch",
     url: "/transaction",
-    icon: TransactionIcon,
+    icon: "bx-transfer",
+  },
+  {
+    text: "Quản lí thẻ",
+    url: "/card",
+    icon: "bx-credit-card",
+  },
+  {
+    text: "Thống kê",
+    url: "/statistical",
+    icon: "bx-pie-chart-alt-2",
+  },
+
+
+  {
+    text: "Tài khoản",
+    url: "/account/profile",
+    icon: "bx-user",
   },
   {
     text: "Đăng xuất",
     url: "/account/logout",
+    icon: 'bx-log-out',
   },
-  
+
 ];
